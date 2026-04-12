8d02798958e2111e038b104278a8c7184eb99772
ChibuikemEgwuChima <chibuikemegwuchima@gmail.com>
2026-04-12 17:24:37 +0100

    hamburger fix

--- public/index.js
+++ public/index.js
@@ -2,9 +2,12 @@ const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 
 burger.addEventListener('click', () => {
-    if(menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
+    menu.classList.toggle('hidden');
+});
+
+window.addEventListener('resize', () =>
+{
+    if (window.innerWidth >= 768) {
         menu.classList.add('hidden');
     }
-})
+});
